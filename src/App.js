6bf32380f9b9baa47f3d10589312bcc9e35ead6e
fandresh62183 <fandresh62183@gmail.com>
2023-11-05 16:35:07 -0500

    componete final

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import "./App.css";
+import { Testimonio } from "./testimonio/Testimonio";
+function App() {
+  return (
+    <div className="App">
+      <div className="contenedor-principal">
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      </div>
+      <Testimonio
+        nombre="Shawn Wang"
+        pais="Singapur"
+        imagen="Shawn"
+        cargo="Ingeniero de software"
+        empresa="Amazon"
+        testimonio="Da miedo cambiar de carrera. Solo gané confianza en que podía codificar trabajando durante cientos de horas de lecciones gratuitas en freeCodeCamp. En un año tenía un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida ."
+      />
+      <Testimonio
+        nombre="Sara Chima"
+        pais="Nigeria"
+        imagen="Sara"
+        cargo="Ingeniero de software"
+        empresa="ChatDesk"
+        testimonio="freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de codificación desde un nivel principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollo en una empresa increíble"
+      />
+
+      <Testimonio
+        nombre="Emma Bostian"
+        pais="Suecia"
+        imagen="Emma"
+        cargo="Ingeniero de software"
+        empresa="Spotify"
+        testimonio="Siempre me ha costado aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."
+      />
+    </div>
+  );
+}
+
+export default App;
